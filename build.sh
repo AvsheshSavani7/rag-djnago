f7a9cbf35b42d709bdec1d00f7c5bfd2e73f6e68
Ashish Kachhadiya <ashish.kachadiya@teqnodux.com>
2025-05-24 12:51:31 +0530

    change requirements.txt path in build command

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Installing dependencies..."
-pip install -r ../requirements.txt
+pip install -r ./requirements.txt
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
