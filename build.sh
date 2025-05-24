0462e8e9989942b4f0ec4a54bf90b1b583e1efda
Ashish Kachhadiya <ashish.kachadiya@teqnodux.com>
2025-05-24 12:54:02 +0530

    add render..yaml to rag_project & change it's path in build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Installing dependencies..."
-pip install -r ./requirements.txt
+pip install -r requirements.txt
 
 echo "Collecting static files..."
+cd rag_project
 python manage.py collectstatic --noinput
 
 echo "Running migrations..."
