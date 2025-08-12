0b40b03bf6a8bfb9f6941350c5cd7659cd4d2131
AvsheshSavani7 <138287046+AvsheshSavani7@users.noreply.github.com>
2025-08-12 16:56:36 +0530

    update- socket bug

--- rag_project/rag_project/asgi.py
+++ rag_project/rag_project/asgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+import socketio
+from rss_feeds.websocket_service import sio
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rag_project.settings")
 
-application = get_asgi_application()
+# application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+
+# Mount Socket.IO at the DEFAULT path "/socket.io"
+application = socketio.ASGIApp(
+    sio,
+    other_asgi_app=django_asgi_app,
+    # socketio_path="socket.io",  # default; keep it default to match clients
+)
