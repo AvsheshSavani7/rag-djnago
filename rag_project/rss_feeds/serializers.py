27bf505dea00dc9d96dd37c6bc15a115e4f2e538
AvsheshSavani7 <138287046+AvsheshSavani7@users.noreply.github.com>
2025-08-13 15:13:59 +0530

    update- serializer thumbnail field validation

--- rag_project/rss_feeds/serializers.py
+++ rag_project/rss_feeds/serializers.py
@@ -73,8 +73,8 @@ class FeedItemCreateSerializer(serializers.Serializer):
     url = serializers.URLField(max_length=1000)
     title = serializers.CharField(max_length=500)
     description_text = serializers.CharField(
-        max_length=2000, required=False, allow_blank=True)
+        max_length=2000, required=False, allow_blank=True, allow_null=True)
     thumbnail = serializers.URLField(
-        max_length=1000, required=False, allow_blank=True)
+        max_length=1000, required=False, allow_blank=True, allow_null=True)
     date_published = serializers.DateTimeField()
-    authors = AuthorSerializer(many=True, required=False)
+    authors = AuthorSerializer(many=True, required=False, allow_null=True)
