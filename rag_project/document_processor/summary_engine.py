96660ab165eb121dcec04912e8c94f04012a7ebb
AvsheshSavani7 <138287046+AvsheshSavani7@users.noreply.github.com>
2026-01-21 11:01:07 +0530

    Refactor summary generation logic by commenting out unused bullet formatting code
    - Commented out the configuration for bullet points in the DOCX summary generation to streamline the code and improve readability.
    - This change does not affect the functionality but prepares the code for potential future adjustments to bullet formatting.

--- rag_project/document_processor/summary_engine.py
+++ rag_project/document_processor/summary_engine.py
@@ -1107,21 +1107,21 @@ def write_docx_summary(summaries, output_path, RUN_CONCISE_SUMMARIES, RUN_FULSOM
 
             add_tab_stop(config_para, 1.0)
 
-            config_bullet = config_para.add_run("○\t")
+            # config_bullet = config_para.add_run("○\t")
 
-            config_bullet.font.name = "Aptos"
+            # config_bullet.font.name = "Aptos"
 
-            config_bullet.font.size = Pt(8)
+            # config_bullet.font.size = Pt(8)
 
-            config_bullet.font.color.rgb = RGBColor(0, 0, 0)
+            # config_bullet.font.color.rgb = RGBColor(0, 0, 0)
 
             # config_bullet = config_para.add_run(
 
             #     "From : " + s.get("clause_name") + " - " + str(s.get("summary_rank")))
 
-            config_bullet.font.name = "Aptos"
+            # config_bullet.font.name = "Aptos"
 
-            config_bullet.font.size = Pt(10)
+            # config_bullet.font.size = Pt(10)
 
     doc.save(output_path)
     print(f"\n✅ DOCX summary written to: {output_path}")
